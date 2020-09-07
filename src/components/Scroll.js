0ebf58c351e8ddbf7eef5519ec35df4a02c42261
NhanNT <iits.williamnhan@gmail.com>
2020-09-06 20:58:10 -0700

    changes scrollbox size, fixes project name

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = ({ children }) => {
   return (
-    <div style={{overflow: 'scroll', border: '0px solid black', height: '400px'}}>
+    <div style={{overflow: 'scroll', border: '0px solid black', height: '600px'}}>
       {children}}
     </div>
   );
