383d7464bda27bb0b143990a3304447facc55c02
NhanNT <iits.williamnhan@gmail.com>
2020-09-06 20:53:19 -0700

    Updates

---
+++ precache-manifest.119e1c7bf8d236731e7e75de1a6a986d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311da140ffbba35dbb293008867ce0e2",
+    "url": "/robotsfriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robotsfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6718114b50e0d07271af",
+    "url": "/robotsfriends/static/css/main.2ed45867.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robotsfriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotsfriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6718114b50e0d07271af",
+    "url": "/robotsfriends/static/js/main.6562035e.chunk.js"
+  },
+  {
+    "revision": "158aef0f383f5fbc4f00",
+    "url": "/robotsfriends/static/js/runtime-main.35b39f12.js"
+  }
+]);
